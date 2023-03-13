86018104a49f77eb78d6bfd527a8305b1c94a803
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-03-13 16:01:06 +0800

    New iteration for handling responses.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use std::{
     io::{self, Read, Write},
     net::TcpStream,
+    time::Duration,
 };
 
 fn msg_server(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> {
@@ -10,24 +11,41 @@ fn msg_server(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> 
 }
 
 // TODO: Handle errors
-fn receive_response(stream: &mut TcpStream) -> Vec<u8> {
-    let mut buf = vec![0; 1024 * 2];
+fn receive_response(stream: &mut TcpStream) -> Vec<char> {
     let mut response = vec![];
+    let mut iter = stream.bytes();
+    while let Some(byte) = iter.next() {
+        if let Err(e) = byte {
+            // So far have not encountered this situation yet.
+            eprintln!("{}", e);
+            todo!("Find out more about the error.");
+        }
 
-    // Will only return Err if no bytes are read.
-    let bytes_read = stream.read(&mut buf).unwrap();
-    response.extend_from_slice(&buf[..bytes_read]);
+        let char = char::from(byte.unwrap());
 
-    // Response code is 3 digit long
-    let mut is_multiline = false;
-    for char in &response[..3] {
-        let value = char::from(char.clone());
-        is_multiline = value.is_ascii_digit();
-    }
+        // Check for CR
+        if char == '\r' {
+            let next_byte = iter.next().unwrap();
+            if let Err(e) = next_byte {
+                // So far have not encountered this situation yet.
+                eprintln!("{}", e);
+                todo!("Find out more about the error.");
+            }
 
-    is_multiline = response[4].to_string() == "-";
-    if is_multiline {
-        // continue reading
+            let next_char = char::from(next_byte.unwrap());
+
+            // Check for LF
+            if next_char == '\n' {
+                // End of response
+                response.push(next_char);
+                break;
+            }
+
+            // If just CR no LF. Continue.
+            response.push(next_char);
+        } else {
+            response.push(char);
+        }
     }
 
     response
@@ -36,21 +54,15 @@ fn receive_response(stream: &mut TcpStream) -> Vec<u8> {
 fn main() -> Result<(), std::io::Error> {
     let mut stream = TcpStream::connect("ftp.gnu.org:21")?;
 
-    loop {
-        let response = receive_response(&mut stream);
-        let s = String::from_utf8_lossy(&response);
+    let response = receive_response(&mut stream);
+    let response: String = response.iter().collect();
+    println!("Server:\n{}", response);
 
-        println!(">>>> {}", s);
-        let mut command = String::new();
-        io::stdin().read_line(&mut command)?;
-
-        if command.trim() == "q" {
-            break;
-        }
-
-        command = format!("{}\r\n", command.trim());
-        msg_server(&mut stream, command.as_bytes())?;
-    }
+    println!("Logging in as anonymous.");
+    msg_server(&mut stream, b"USER anonymous")?;
+    let response = receive_response(&mut stream);
+    let response: String = response.iter().collect();
+    println!("Server:\n{}", response);
 
     Ok(())
 }
