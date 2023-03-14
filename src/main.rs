c187da317dc1850053cc45bd77dcf7c3a1183119
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-03-14 13:12:14 +0800

    Applying stash

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,37 @@
 use std::{
     io::{self, Read, Write},
     net::TcpStream,
-    time::Duration,
 };
 
-fn msg_server(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> {
-    stream.write_all(msg)
+fn send_msg(stream: &mut TcpStream, msg: &str) {
+    let msg = format!("{}\r\n", msg);
+    stream.write_all(msg.as_bytes()).unwrap();
 }
 
 // TODO: Handle errors
-fn receive_response(stream: &mut TcpStream) -> Vec<char> {
-    let mut response = vec![];
-    let mut iter = stream.bytes();
-    while let Some(byte) = iter.next() {
-        if let Err(e) = byte {
-            // So far have not encountered this situation yet.
-            eprintln!("{}", e);
-            todo!("Find out more about the error.");
-        }
+fn receive_response(stream: &mut TcpStream) -> String {
+    let mut buffer = vec![0u8; 1024 * 8]; // this should be more than enough for a message
 
-        let char = char::from(byte.unwrap());
+    let data = match stream.read(&mut buffer) {
+        Ok(0) => panic!("connection closed"),
+        Ok(n) => &buffer[..n],
+        Err(e) => panic!("{e}"),
+    };
 
-        // Check for CR
-        if char == '\r' {
-            let next_byte = iter.next().unwrap();
-            if let Err(e) = next_byte {
-                // So far have not encountered this situation yet.
-                eprintln!("{}", e);
-                todo!("Find out more about the error.");
-            }
-
-            let next_char = char::from(next_byte.unwrap());
-
-            // Check for LF
-            if next_char == '\n' {
-                // End of response
-                response.push(next_char);
-                break;
-            }
-
-            // If just CR no LF. Continue.
-            response.push(next_char);
-        } else {
-            response.push(char);
-        }
-    }
-
-    response
+    std::str::from_utf8(data).unwrap().to_string()
 }
 
 fn main() -> Result<(), std::io::Error> {
     let mut stream = TcpStream::connect("ftp.gnu.org:21")?;
 
-    let response = receive_response(&mut stream);
-    let response: String = response.iter().collect();
-    println!("Server:\n{}", response);
-
     println!("Logging in as anonymous.");
-    msg_server(&mut stream, b"USER anonymous")?;
+    send_msg(&mut stream, "USER anonymous");
     let response = receive_response(&mut stream);
-    let response: String = response.iter().collect();
-    println!("Server:\n{}", response);
+    println!("\nServer:\n{}", response);
+
+    send_msg(&mut stream, "HELP");
+    let response = receive_response(&mut stream);
+    println!("\nServer:\n{}", response);
 
     Ok(())
 }
